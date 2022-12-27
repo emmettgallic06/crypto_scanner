f039264783d86c52f8f6e69623106ad2fbc23007
mikeyhardy14 <98924283+mikeyhardy14@users.noreply.github.com>
2022-12-26 23:20:13 -0700

    Pandas Data File to CSV

--- python/scan.py
+++ python/scan.py
@@ -3,9 +3,12 @@ import pandas as pd
 import numpy as np
 import csv
 
+APIKEY = "G9JUJVPPH7N45HJGZNB69YM1Z93GM4RE83"
+address = "0x02F70172F7f490653665C9bFAc0666147c8aF1F5"
 data = []
-with PolygonScan("G9JUJVPPH7N45HJGZNB69YM1Z93GM4RE83",False) as matic:
-    data.append(matic.get_matic_balance(address="0x02F70172F7f490653665C9bFAc0666147c8aF1F5"))
+
+with PolygonScan(APIKEY,False) as matic:
+    data.append(matic.get_matic_balance(address=address))
 
 df = pd.DataFrame(data)
 df.to_csv('../output/results.csv', index= False, header=False)

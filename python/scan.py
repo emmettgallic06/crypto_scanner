eb48fdf25d6dcda39a4becc77df4edb5f4f37abb
mikeyhardy14 <98924283+mikeyhardy14@users.noreply.github.com>
2022-12-27 19:46:07 -0700

    Timestamp implemented

--- python/scan.py
+++ python/scan.py
@@ -1,19 +1,23 @@
 from polygonscan import PolygonScan
 import pandas as pd
+import datetime
 
+current_time = str(int(datetime.datetime.now().timestamp()-84))
 APIKEY = "G9JUJVPPH7N45HJGZNB69YM1Z93GM4RE83"
 address = "0x02F70172F7f490653665C9bFAc0666147c8aF1F5"
-data = []
+data = {'Address':[address],
+        'Timestamp': [],
+        'Amount': []}
 
 with PolygonScan(APIKEY,False) as matic:
     # Grab Amount
-    data.append(matic.get_matic_balance(address=address))
+    data['Amount'].append(matic.get_matic_balance(address=address))
 
     # Grab TimeStamp
-    data.append(matic.get_block_number_by_timestamp(timestamp="1671949596", closest="before"))
+    data['Timestamp'].append(matic.get_block_number_by_timestamp(timestamp=current_time, closest="before"))
 
 # Convert to Pandas Dataframe
 df = pd.DataFrame(data)
 
 # Convert to CSV File
-df.to_csv('../output/results.csv', index= False, header=False)
+df.to_csv('../output/results.csv', index= False, header=True)

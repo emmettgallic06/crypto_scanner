a042b05be149c66da792d9175ed1926667395165
mikeyhardy14 <98924283+mikeyhardy14@users.noreply.github.com>
2023-01-02 22:06:12 -0700

    More flask additions

--- python/main.py
+++ python/main.py
@@ -1,5 +1,7 @@
 import webapp
 
+
 if __name__ == '__main__':
     print("Launching Crypto-Scanner...")
     webapp.app.run()
+
--- python/webapp.py
+++ python/webapp.py
@@ -1,5 +1,21 @@
-from flask import Flask,render_template, request,json
+from flask import Flask,render_template, request,json,flash,redirect,url_for
 app =Flask(__name__)
+messages = [{'contract': 'Message One'}
+            ]
+@app.route("/find", methods =('GET','POST'))
+def find():
+    if request.method == 'POST':
+        contract = request.form['contract']
+        startdate = request.form['start']
+        enddate = request.form['end']
+        time = request.form['time']
+        timestamp = request.form['timestamp']
+        amount = request.form['Amount']
+        if not contract:
+            flash('Contract is required')
+        else:
+            messages.append({'contract': contract})
+            return redirect(url_for('index'))
 
 @app.route("/")
 def main():

f9000adf3df470e006eb39c4f277d844ec517f03
mikeyhardy14 <98924283+mikeyhardy14@users.noreply.github.com>
2023-01-17 09:59:58 -0700

    str8_api refactor

--- python/str8_api.py
+++ python/str8_api.py
@@ -1,43 +1,35 @@
 import requests
 import json
 
+def get_data(self, api):
+    response = requests.get(f"{api}")
+    if response.status_code == 200:
+        print("sucessfully fetched the data")
+        self.formatted_print(response.json())
+    else:
+        print(
+            f"Hello person, there's a {response.status_code} error with your request")
 
-class MakeApiCall:
+def get_user_data(api, parameters):
+    response = requests.get(f"{api}", params=parameters)
+    if response.status_code == 200:
+        print("sucessfully fetched the data with parameters provided")
+        return formatted_print(response.json())
+    else:
+        print(
+            f"Hello person, there's a {response.status_code} error with your request")
 
-    def get_data(self, api):
-        response = requests.get(f"{api}")
-        if response.status_code == 200:
-            print("sucessfully fetched the data")
-            self.formatted_print(response.json())
-        else:
-            print(
-                f"Hello person, there's a {response.status_code} error with your request")
-
-    def get_user_data(self, api, parameters):
-        response = requests.get(f"{api}", params=parameters)
-        if response.status_code == 200:
-            print("sucessfully fetched the data with parameters provided")
-            self.formatted_print(response.json())
-        else:
-            print(
-                f"Hello person, there's a {response.status_code} error with your request")
-
-    def formatted_print(self, obj):
-        text = json.dumps(obj, sort_keys=True, indent=4)
-        print(text)
-
-    def __init__(self, api):
-        # self.get_data(api)
-
-        parameters = {
-            "apikey": "G9JUJVPPH7N45HJGZNB69YM1Z93GM4RE83",
-            "module": "account",
-            "action": "txlist",
-            "address": "0x5A534988535cf27a70e74dFfe299D06486f185B7"
-        }
-
-        self.get_user_data(api, parameters)
+def formatted_print( obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
 
 
-if __name__ == "__main__":
-    api_call = MakeApiCall("https://api.polygonscan.com/api")
+parameters = {
+    "apikey": "G9JUJVPPH7N45HJGZNB69YM1Z93GM4RE83",
+    "module": "account",
+    "action": "txlist",
+    "address": "0x5A534988535cf27a70e74dFfe299D06486f185B7"
+}
+
+api_call = get_user_data("https://api.polygonscan.com/api",parameters)
+print(api_call)

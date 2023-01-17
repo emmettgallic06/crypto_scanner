666a46a2bd035aa6400c2619caf7add3b64b6a47
mikeyhardy14 <98924283+mikeyhardy14@users.noreply.github.com>
2023-01-17 10:01:33 -0700

    str8_api refactor

--- python/str8_api.py
+++ python/str8_api.py
@@ -5,7 +5,7 @@ def get_data(self, api):
     response = requests.get(f"{api}")
     if response.status_code == 200:
         print("sucessfully fetched the data")
-        self.formatted_print(response.json())
+        return formatted_print(response.json())
     else:
         print(
             f"Hello person, there's a {response.status_code} error with your request")

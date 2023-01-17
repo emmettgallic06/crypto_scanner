36bff65be3696787a712defbda2ad937a1ab8750
Emmett Gallic <emmettgallic@gmail.com>
2023-01-17 12:00:23 -0500

    changes

--- python/str8_api.py
+++ python/str8_api.py
@@ -4,15 +4,6 @@ import json
 
 class MakeApiCall:
 
-    def get_data(self, api):
-        response = requests.get(f"{api}")
-        if response.status_code == 200:
-            print("sucessfully fetched the data")
-            self.formatted_print(response.json())
-        else:
-            print(
-                f"Hello person, there's a {response.status_code} error with your request")
-
     def get_user_data(self, api, parameters):
         response = requests.get(f"{api}", params=parameters)
         if response.status_code == 200:
